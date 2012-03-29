b8e39823afbf477a4d261724ee1e61f523ad06d9
Paul Groudas <me@paulgroudas.com>
2012-03-29 00:43:58 -0400

    PJ: Adding skeleton files

---
+++ tests/filestore/s3_test.py
@@ -0,0 +1,10 @@
+import gmailcompressor.filestore.s3store
+import unittest
+
+class S3StoreTestCase(unittest.TestCase):
+
+    def test_test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
